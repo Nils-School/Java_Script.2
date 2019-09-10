aba2e0901d73910ff814feed816f14f73a4a4d7e
NIlsSchnitzel <235770@student.insulacollege.nl>
2019-09-10 12:26:39 +0200

    Java_Script.2

---
+++ b1w2l2.js
@@ -0,0 +1,16 @@
+alert(' Hallo Rompie'); // pop up
+document.write('Voornaam en achternaam:')
+document.write(naam); // Zegt je naam
+
+document.write('<br/>Leeftijd:')
+document.write(leeftijd)
+
+document.write ('<br/>Naam opleiding:');
+document.write(opleiding)
+document.write('<br/>')
+
+var opleiding =prompt('Naam opleiding');// pop up
+var leeftijd = prompt('Vul je leeftijd in');// pop up
+var naam = prompt('Vul je naam in');// pop up
+
+
